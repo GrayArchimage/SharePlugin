ba65edcca39355a9665ee354ccc700978ae45798
GrayArchimage <2300233726@qq.com>
2023-05-18 14:48:24 +0800

    提交登录

--- Assets/Plugins/iOS/WeChatLogin/WXApiManager.mm
+++ Assets/Plugins/iOS/WeChatLogin/WXApiManager.mm
@@ -28,6 +28,41 @@
     	}
 }
 
+// 通过code换取token，此步实际项目是放在服务端执行
+-(void)requestWxToken:(NSString*)code {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.weixin.qq.com/sns/oauth2/access_token?appid=%@&secret=%@&code=%@&grant_type=authorization_code", mWXAppid, "你的APPSecret", code]];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error == nil) {
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            
+            [self requestWxUserInfo:[dict valueForKey:@"access_token"]];
+        }
+    }];
+    
+    [dataTask resume];
+}
+
+// 通过token查询用户信息，此步实际项目是放在服务端执行
+-(void)requestWxUserInfo:(NSString*)token{
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.weixin.qq.com/sns/userinfo?access_token=%@&openid=%@",token, mWXAppid]];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error == nil) {
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            NSString *jsonStr = [self DataTOjsonString:dict];
+            // 回调给Unity
+            UnitySendMessage("SDKCallBack", "WeChatLoginCallback",[jsonStr UTF8String]);
+        }
+    }];
+    
+    [dataTask resume];
+}
+
 - (void)onReq:(BaseReq *)req {
 	// TODO 微信回调，从微信端主动发送过来的请求
 }
